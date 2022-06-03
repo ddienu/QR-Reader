f0c49373b8137c868d5192a3302e5a83c3f33358
ddienu <rockdann@hotmail.com>
2022-06-03 02:47:42 -0500

    Add: Home and Mapa Page.
    Start the aplication QR Reader with home and mapa page, initial routes and a container

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:qr_reader/src/pages/home_page.dart';
+import 'package:qr_reader/src/pages/mapa_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomePage(),
+        'mapa': (_) => MapaPage(),
+      },
+    );
+  }
+}
---
+++ lib/src/pages/mapa_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class MapaPage extends StatelessWidget {
+  const MapaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Mapa Page'),
+    );
+  }
+}
